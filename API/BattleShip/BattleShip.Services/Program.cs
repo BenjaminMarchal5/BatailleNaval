dcf66c211c51f66dccc9beaa1a2eb233937451b5
tritschbergera <antoine.tritschberger@etu.unistra.fr>
2022-04-18 11:42:57 +0200

    Ajout id

---
+++ API/BattleShip/BattleShip.Services/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleShip.Services
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
