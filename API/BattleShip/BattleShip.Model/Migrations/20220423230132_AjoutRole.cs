91a1e5090030cb471c61bb41011488e41f2c5e2e
tritschbergera <antoine.tritschberger@etu.unistra.fr>
2022-04-24 01:02:26 +0200

    Modif

---
+++ API/BattleShip/BattleShip.Model/Migrations/20220423230132_AjoutRole.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BattleShip.Model.Migrations
+{
+    public partial class AjoutRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Players_PlayerId",
+                table: "Games");
+
+            migrationBuilder.RenameColumn(
+                name: "Size",
+                table: "RequiredShip",
+                newName: "SizeShip");
+
+            migrationBuilder.RenameColumn(
+                name: "PlayerId",
+                table: "Games",
+                newName: "WinnerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Games_PlayerId",
+                table: "Games",
+                newName: "IX_Games_WinnerId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberShip",
+                table: "RequiredShip",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Players_WinnerId",
+                table: "Games",
+                column: "WinnerId",
+                principalTable: "Players",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Players_WinnerId",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "NumberShip",
+                table: "RequiredShip");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Players");
+
+            migrationBuilder.RenameColumn(
+                name: "SizeShip",
+                table: "RequiredShip",
+                newName: "Size");
+
+            migrationBuilder.RenameColumn(
+                name: "WinnerId",
+                table: "Games",
+                newName: "PlayerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Games_WinnerId",
+                table: "Games",
+                newName: "IX_Games_PlayerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Players_PlayerId",
+                table: "Games",
+                column: "PlayerId",
+                principalTable: "Players",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ API/BattleShip/BattleShip.Model/Model/Position.cs
@@ -0,0 +1,37 @@
+﻿using BattleShip.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.Model.Model
+{
+    public class Position : IStoredObject
+    {
+        public int Id { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position() { }
+        public Position(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj is Position pos)
+            {
+                return pos.X == this.X && pos.Y == this.Y;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.X + this.Y;
+        }
+    }
+}
